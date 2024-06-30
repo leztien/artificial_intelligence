0225725db16d8d31403bb8fe07876150d0f03451
Ilja Neitzel <48551497+leztien@users.noreply.github.com>
2024-06-30 20:54:27 +0200

    Update _working.py

--- _working.py
+++ _working.py
@@ -1,133 +1,185 @@
+#!/usr/bin/env python3
 
-
-# AIMA_page_095_bfs_vs_dfs.py
+#AIMA_page_116_eight_puzzle.py
 
 """
-Breadth Firs search
-iterative Depth First search
-recursive Depth First search
+Solve an 8-puzzle with A* (with the Manhatten heuritic)
+
+State is just a Python 1D tuple
 """
 
 
+import random
+from math import sqrt
+import matplotlib.pyplot as plt
+
+from enum import Enum, auto
 
 from abc import ABC, abstractmethod, abstractproperty
 from typing import NamedTuple, Union, Optional, Any
 from dataclasses import dataclass
+from math import atan, degrees
+from math import sqrt, floor, ceil
+from itertools import product
 from functools import reduce
 from operator import or_
 import numpy as np
 import matplotlib.pyplot as plt
-import networkx as nx
 
 
 
-##### Utilities to generate a random graph and the display thtereof #####
 
-def generate_graph(n, seed=None):
-    """
-    Generate a random prahp in this form:
-        graph = {'vertices': {A, B, C, D}, 'edges': {{A,B}, {B,C}}}
-    """
+# 1. Generate a random state
+def generate(puzzle_name='eight', random_state=None):
+    """."""
+    if puzzle_name not in ('eight', 'fifteen'):
+        raise ValueError("bad puzzle name")
     
-    # limitations
-    if n > 26:
-        raise ValueError("n must be less that or equal tomthe nmber of the letters innthe latin alphabet")
+    random.seed(random_state)
+    state = list(range({'eight': 9, 'fifteen': 16}[puzzle_name]))
+    random.shuffle(state)
+    return tuple(state)
     
-    # initialize a graph object
-    graph= dict(vertices={chr(65+i) for i in range(n)}, edges=set())
-    
-    # Random seed
-    if seed is True:
-        seed = np.random.randint(1, 9999)
-        print("seed =", seed)
-    rs = np.random.RandomState(seed)
-    
-    # make a random adj mx
-    p = np.log(n) / 10 / n
-    while True:
-        mx = np.triu(rs.rand(n,n))
-        mx += mx.T
-        mx = (mx <= p).astype("uint8")
-        mx[np.diag_indices(n)] = 0
+
+
+# 2. Validate a state whetehr it is valid and solvable
+def validate(state):
+    raise NotImplementedError("This function is not yet implemented")
+
+
+
+# 3. Display a state
+def display(state):
+    """Display a state"""
+    n = len(state)
+    ax = plt.axes()
+    d = [1/sqrt(n)*i for i in range(int(sqrt(n))+1)]
+    ax.vlines(d, ymin=0.0, ymax=1.0, color='k')
+    ax.hlines(d, xmin=0.0, xmax=1.0, color='k')
         
-        if mx.sum(axis=0).min() > 0: break
-        else: p += 0.001
-            
-    # convert the adj mx into edges dict
-    for i,row in enumerate(np.triu(mx)[:-1]):
-        for j in np.nonzero(row)[0]:
-            graph['edges'].add(frozenset({chr(65+i), chr(65+j)}))
-    return graph
-            
+    d = [1/(int(sqrt(n)*2))*i for i in range(1, int(sqrt(n)*2), 2)]
 
-def display_graph(graph):
+    for ix, value in enumerate(state):
+        if value==0:
+            continue
+        i,j = divmod(ix, int(sqrt(n)))
+        ax.annotate(text=value, xy=(d[j], d[int(sqrt(n))-1-i]),
+                    fontsize='xx-large', fontweight='bold',
+                    horizontalalignment='center', verticalalignment='center')
+    ax.set_aspect('equal', 'box')
+    ax.axis('off')
+    return ax
+
+
+
+
+# 4. Data structures for the problem reresentation
+
+class PriorityQueue:
     """
-    Vizualize a graph passed in in this form:
-    graph = {'vertices': {A, B, C, D}, 'edges': {{A,B}, {B,C}}}
+    priority queue implemented with a binary heap.
+    Accepts an evaluation function f to compare the nodes
     """
+    def __init__(self, evaluation_function):
+        self._nodes = []
+        if not callable(evaluation_function):
+            raise TypeError("The evaluation_function must be callable")
+        self.f = evaluation_function
+
+    def __len__(self):
+        return len(self._nodes)
     
-    G = nx.Graph()
-    G.add_nodes_from(graph['vertices'])
-    G.add_edges_from(graph['edges'])
+    def is_empty(self):
+        return len(self._nodes) == 0
+
+    def add(self, item):
+        # alias for convenience
+        nodes = self._nodes
         
-    # add coloring to the nodes
-    color_map = []
-    for node in G:
-        if node == min(graph['vertices']):
-            color_map.append('green')
-        elif node == max(graph['vertices']):
-            color_map.append('red')
-        else: 
-            color_map.append('cyan')
+        # append to the end
+        nodes.append(item)
+        
+        # bubble up
+        c = len(nodes) - 1
+        while c:
+            p = (c - (1 if c%2 else 2)) // 2
+            if self.f(nodes[c]) < self.f(nodes[p]):
+                nodes[c], nodes[p] = nodes[p], nodes[c]
+                c = p
+            else: break
     
-    nx.draw(G, with_labels=True, node_color=color_map,)
-    plt.show()
-    return G
+    def top(self):
+        """returns the top element but does not remove it, unlike pop()"""
+        return self._nodes[0]
     
-    
-    
-#### Data Structures to fomralize the problem #####
-
-class AbstractQueue(ABC):
-    """Abstract Queue for FIFO and LIFO queues"""
-    def __init__(self, items=None): self._items = list(items or [])
-    def __repr__(self): return f"{self.__class__.__name__}({str(self._items)[1:-1]})"
-    def __getitem__(self, index): return self._items[index]
-    def __delitem__(self, index): del self._items[index]
-    def __len__(self): return len(self._items)
-    def is_empty(self): return len(self) == 0
-    def add(self, item): self._items.append(item)
-    @abstractmethod
     def pop(self):
-        "pop-right for stack bzw pop-left for queue"
-        if len(self) == 0: raise IndexError("The queue is empty")
-
-
-class Queue(AbstractQueue):
-    """FIFO queue"""
-    def pop(self):
-        super().pop()
-        item = self[0]
-        del self[0]
+        # alias for convenience
+        nodes = self._nodes
+        
+        # swap
+        nodes[0], nodes[-1] = nodes[-1], nodes[0]
+        
+        # pop
+        item = nodes.pop()
+        
+        # bubble down
+        p = 0
+        while True:
+            # determine the indeces
+            l = (2*p + 1) if (2*p + 1) < len(nodes) else None
+            r = (2*p + 2) if (2*p + 2) < len(nodes) else None
+            c = r if (l and r) and (self.f(nodes[r]) < self.f(nodes[l])) else l
+            
+            # swap if necessary
+            if c and self.f(nodes[c]) < self.f(nodes[p]):
+                nodes[p], nodes[c] = nodes[c], nodes[p]
+                p = c
+            else: break
         return item
 
 
-class Stack(AbstractQueue):
-    """LIFO queue i.e. stack"""
-    def pop(self):
-        super().pop()
-        return self._items.pop()
 
+class Action(Enum):
+    RIGHT = (0, 1)
+    LEFT = (0, -1)
+    TOP = (-1, 0)
+    BOTTOM = (1, 0)
 
+  
+"""
 @dataclass
 class Node:
-    state: Union[int, str, Any]
+    state: Union[list, tuple]
     parent: Optional['Node'] = None
-    action: Optional[Any] = None
+    action: Action = None
     path_cost: Optional[float] = 0.0
     heuristic: Optional[float] = float('inf')
     def __repr__(self): return "{}({})".format(self.__class__.__name__, self.state)
+"""
 
+
+
+
+class Node:
+    def __init__(self, 
+                    state: Union[list, tuple],
+                    parent: Optional['Node'] = None,
+                    action: Action = None,
+                    path_cost: Optional[float] = 0.0,
+                    heuristic: Optional[float] = float('inf')
+                    ):
+        self.state = tuple(state)  # to ensure imutability
+        self.parent = parent
+        self.action = action
+        self.path_cost = path_cost
+        self.heuristic = heuristic
+
+    def __repr__(self): 
+        return "{}({})".format(self.__class__.__name__, str(self.state)[1:-1])
+
+
+
+# 5. Problem representation
 
 class AbstractSearchProblem(ABC):
     """
@@ -152,55 +204,57 @@ class AbstractSearchProblem(ABC):
     def result(self, state, action):
         "transition model"
     
-    @abstractmethod
-    def action_cost(state, action, next_state) -> float:
+    def action_cost(self, state, action, next_state) -> float:
         "returns the cost of performing an action from the current state that takes the agent to the next_state"
+        return 1.0
+    
+    def __repr__(self):
+        return f"{self.__class__.__name__}()"
+
 
 
 class SearchProblem(AbstractSearchProblem):
     """
     Concrete search problem representation and implementation.
     """
-    def __init__(self, problem_representation):
+    def __init__(self, initial_state):
         """
-        Expects a graph as problem representation, in this format:
-        graph = {'vertices': {A, B, C, D}, 'edges': {{A,B}, {B,C}}}
         """
-        self.graph = problem_representation
-        #self.graph['edges'] = {frozenset(k):v for k,v in self.graph['edges'].items()}
-
+        self._initial_state = initial_state
+        self.empty_tile = 0
+        self.side_length = int(sqrt(len(self._initial_state)))
+        
     @property
     def initial(self):
-        """Returns the INITIAL state"""
-        return min(self.graph['vertices'].keys())
+        return self._initial_state
     
     def is_goal(self, state) -> bool:
-        return state == max(self.graph['vertices'].keys())
+        return tuple(state) == tuple(range(len(self._initial_state)))
     
     def actions(self, state) -> set:
-        return reduce(or_, {edge for edge in self.graph['edges'] if state in edge}, frozenset()).difference({state})
+        i,j = divmod(state.index(self.empty_tile), self.side_length)
+        
+        candidates = {action: (i + action.value[0], j + action.value[1]) 
+                      for action in Action}
+        return {action for action, coords in candidates.items() 
+                if min(coords) >= 0 and max(coords) < self.side_length}
         
     def result(self, state, action):
-        """transition model"""
-        next_state = action  # here
-        edge = frozenset({state, action})
-        return next_state if edge in self.graph['edges'] else None
+        zero_tile_index = state.index(self.empty_tile)
+        i,j = divmod(state.index(self.empty_tile), self.side_length)
+        i += action.value[0]
+        j += action.value[1]
+        assert min(i,j) >= 0 and max(i,j) < self.side_length, "bad action"
+        swap_tile_index = i * self.side_length + j
+        new_state = list(state)
+        new_state[zero_tile_index], new_state[swap_tile_index] = new_state[swap_tile_index], new_state[zero_tile_index]
+        return tuple(new_state)
     
-    def action_cost(self, state, action, next_state=None) -> float:
-        assert next_state is None or next_state == action, "action must be == next_state in this problem type implementation"
-        edge = frozenset({state, action})
-        return self.graph['edges'][edge]
-
     def heuristic(self, state):
-        """get the heuristic value from the given node to the GOAL"""
-        state_coords = np.array(self.graph['vertices'][state])
-        goal_coords = np.array(self.graph['vertices'][max(self.graph['vertices'])])
-        return ((goal_coords - state_coords) ** 2).sum() ** 0.5
+        """the h2 heuristic as described in AIMA on page 116"""
+        return 1.0
         
     def expand(self, node):
-        """
-        note: in AIMA, expand() is a stand-alone function
-        """
         state = node.state
         for action in self.actions(state):
             next_state = self.result(state, action)
@@ -209,52 +263,58 @@ class SearchProblem(AbstractSearchProblem):
             yield Node(state=next_state, parent=node, action=action, 
                        path_cost=path_cost, heuristic=heuristic)
     
-    def get_solution_path(self, node, return_cost=False):
+    def get_solution_path(self, node):
         assert self.is_goal(node.state), "node must be the GOAL"
         path = [node.state,]
         while node.state != self.initial:
             node = node.parent
             path.append(node.state)
-        path = path[::-1]
+        return path[::-1]
         
-        if return_cost:
-            edges = [frozenset({path[i], path[i+1]}) for i in range(len(path)-1)]
-            cost = sum(self.graph['edges'][edge] for edge in edges)
-        
-        return (path, cost) if return_cost else path
-    
-
- 
 
 
 
 
+state = generate('fifteen')
 
-##### The most important part - the search functions themselves #####
+node = Node(state)
 
-def search(problem, search_type="bfs"):
-    ...
-
-
-
-def recursive_dfs(problem):
-    ...
+display(state)
 
 
 
+problem = SearchProblem(initial_state=state)
+
+
+"""
+actions = problem.actions(state)
+print(actions)
+
+for action in actions:
+    new = problem.result(state, action)
+    plt.figure()
+    display(new)
+"""
+
+
+for node in problem.expand(node):
+    print(node)
+    plt.figure()
+    display(node.state)
 
 
 
+
+# 6. Search algorithm
+...
+
+
+
+
+
+#7. Demo test
 if __name__ == '__main__':
-    # Demo
-    graph = generate_graph(n=10, seed=True)
+    ...
     
-    display_graph(graph)
-    
-    
-    
-    
-
-
 
 
